3248b6c242054b983b25957031bbff4ee4f5a369
Nyrexon <91697233+Nyrexon@users.noreply.github.com>
2022-09-08 20:54:57 +0300

    Create Author.java

---
+++ Author.java
@@ -0,0 +1,33 @@
+package nyrex;
+
+public class Author {
+    private String name, email;
+    private char gender;
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "автор - " + this.name + " (" + this.gender + ") at " + this.email;
+    }
+}
